f570379db0db8e642a0e13ad84fe5ebacc0286d3
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-28 11:33:49 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
+
+function App() {
+
+  return (
+    <>
+      <h1>Boolflix</h1>
+    </>
+  );
+};
+
+export default App
