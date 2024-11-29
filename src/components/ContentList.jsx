492c9d75ef247481112042de84cd46cdbdcf38e0
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-29 10:24:18 +0100

    add header, main, footer and logo

--- src/components/ContentList.jsx
+++ src/components/ContentList.jsx
@@ -36,57 +36,70 @@ export const ContentList = () => {
     // render
     return (
         <>
-            {/* Ricerca film */}
-            <form onSubmit={handleSearchSubmit}>
+            <header>
 
-                {/* Input per la ricerca dei contenuti */}
-                <input
-                    type="search"
-                    placeholder="Search..."
-                    value={searchContent}
-                    onChange={e => { setSearchContent(e.target.value) }}
-                />
-                {/* Pulsante per avviare la ricerca */}
-                <button
-                    type="submit"
-                >Cerca</button>
+                <img src="/logo.png" alt="" />
 
-            </form>
+                {/* Ricerca film */}
+                <form onSubmit={handleSearchSubmit}>
 
+                    {/* Input per la ricerca dei contenuti */}
+                    <input
+                        type="search"
+                        placeholder="Search..."
+                        value={searchContent}
+                        onChange={e => { setSearchContent(e.target.value) }}
+                    />
+                    {/* Pulsante per avviare la ricerca */}
+                    <button
+                        type="submit"
+                    >Cerca</button>
 
-            {/* Lista film */}
-            <ul>
+                </form>
 
-                {data.length > 0 ? (
-                    data.map((movie) => (
-                        <li key={movie.id}>
+            </header>
 
-                            <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
-                            <h5>{movie.title ? movie.title : movie.name}</h5>
-                            <p>{movie.original_title ? movie.original_title : movie.original_name}</p>
-                            <p>
-                                <ReactCountryFlag
-                                    countryCode={languageFlag[movie.original_language] || "US"}
-                                />
-                            </p>
+            <main>
 
-                            {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle */}
-                            {Array.from({ length: Math.ceil(movie.vote_average / 2) }).map((_, index) => (
-                                <FontAwesomeIcon key={`full-${index}`} icon={faStar} />
-                            ))}
+                {/* Lista film */}
+                <ul>
 
-                            {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle vuote */}
-                            {Array.from({ length: Math.floor((10 - movie.vote_average) / 2) }).map((_, index) => (
-                                <FontAwesomeIcon key={`empty-${index}`} icon={faStarEmpty} />
-                            ))}
+                    {data.length > 0 ? (
+                        data.map((movie) => (
+                            <li key={movie.id}>
 
-                        </li>
-                    ))
-                ) : (
-                    <li>No movies found</li>
-                )}
+                                <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
+                                <h5>{movie.title ? movie.title : movie.name}</h5>
+                                <p>{movie.original_title ? movie.original_title : movie.original_name}</p>
+                                <p>
+                                    <ReactCountryFlag
+                                        countryCode={languageFlag[movie.original_language] || "US"}
+                                    />
+                                </p>
 
-            </ul >
+                                {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle */}
+                                {Array.from({ length: Math.ceil(movie.vote_average / 2) }).map((_, index) => (
+                                    <FontAwesomeIcon key={`full-${index}`} icon={faStar} />
+                                ))}
+
+                                {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle vuote */}
+                                {Array.from({ length: Math.floor((10 - movie.vote_average) / 2) }).map((_, index) => (
+                                    <FontAwesomeIcon key={`empty-${index}`} icon={faStarEmpty} />
+                                ))}
+
+                            </li>
+                        ))
+                    ) : (
+                        <li>No movies found</li>
+                    )}
+
+                </ul >
+
+            </main>
+
+            <footer>
+                <p>© 2024 - All Rights Reserved</p>
+            </footer>
         </>
     );
 };
