90067a21409a1f9602f1f8086b7f011003f7e834
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-02 18:16:06 +0100

    fix language flag

--- src/components/ContentList.jsx
+++ src/components/ContentList.jsx
@@ -5,20 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarEmpty } from "@fortawesome/free-regular-svg-icons";
 
-
-const languageFlag = {
-    it: "IT",
-    en: "US",
-    fr: "FR",
-    es: "ES",
-    ja: "JP",
-    gb: "GB",
-    de: "DE",
-    cn: "CN",
-    ko: "KR",
-    ru: "RU"
-};
-
 export const ContentList = () => {
     // logic
 
@@ -29,59 +15,56 @@ export const ContentList = () => {
     // render
     return (
         <>
-            <>
-                <div className="container">
-                    <div className="row row-cols row-cols-md-2 row-cols-lg-3 g-3">
+            <div className="container">
+                <div className="row row-cols row-cols-md-2 row-cols-lg-3 g-3">
 
-                        {/* Lista film */}
-                        {data.length > 0 ? (
-                            data.map((movie) => (
+                    {/* Lista film */}
+                    {data.length > 0 ? (
+                        data.map((movie) => (
 
-                                <div className="col" key={movie.id}>
-                                    <div
-                                        className="card text-bg-dark"
-                                        style={
-                                            {
-                                                backgroundImage: `url(https://image.tmdb.org/t/p/w342${movie.poster_path})`,
-                                            }
+                            <div className="col" key={movie.id}>
+                                <div
+                                    className="card text-bg-dark"
+                                    style={
+                                        {
+                                            backgroundImage: `url(https://image.tmdb.org/t/p/w342${movie.poster_path})`,
                                         }
-                                    >
-                                        <div className="card-body">
+                                    }
+                                >
+                                    <div className="card-body">
 
-                                            <h5 className="card-title">{movie.title ? movie.title : movie.name}</h5>
-                                            <span className="card-text me-2">{movie.original_title ? movie.original_title : movie.original_name}</span>
-                                            <span className="card-text">
-                                                <ReactCountryFlag
-                                                    countryCode={languageFlag[movie.original_language] || "US"}
-                                                />
-                                            </span>
+                                        <h5 className="card-title">{movie.title ? movie.title : movie.name}</h5>
+                                        <span className="card-text me-2">{movie.original_title ? movie.original_title : movie.original_name}</span>
+                                        <span className="card-text">
+                                            <ReactCountryFlag countryCode={movie.original_language ? movie.original_language.toUpperCase() : "US"} />
+                                        </span>
 
-                                            <div className="average">
+                                        <div className="average">
 
-                                                {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle */}
-                                                {Array.from({ length: Math.ceil(movie.vote_average / 2) }).map((_, index) => (
-                                                    <FontAwesomeIcon key={`full-${index}`} icon={faStar} />
-                                                ))}
+                                            {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle */}
+                                            {Array.from({ length: Math.ceil(movie.vote_average / 2) }).map((_, index) => (
+                                                <FontAwesomeIcon key={`full-${index}`} icon={faStar} />
+                                            ))}
 
-                                                {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle vuote */}
-                                                {Array.from({ length: Math.floor((10 - movie.vote_average) / 2) }).map((_, index) => (
-                                                    <FontAwesomeIcon key={`empty-${index}`} icon={faStarEmpty} />
-                                                ))}
-                                            </div>
-
-                                            <p className="card-text overview">{movie.overview}</p>
-
+                                            {/* Creo un array della lunghezza del voto / 2 poi ci ciclo per stmpare le stelle vuote */}
+                                            {Array.from({ length: Math.floor((10 - movie.vote_average) / 2) }).map((_, index) => (
+                                                <FontAwesomeIcon key={`empty-${index}`} icon={faStarEmpty} />
+                                            ))}
                                         </div>
 
+                                        <p className="card-text overview">{movie.overview}</p>
+
                                     </div>
+
                                 </div>
-                            ))
-                        ) : (
-                            <h5 className="card-title">No movies found</h5>
-                        )}
-                    </div>
+                            </div>
+                        ))
+                    ) : (
+                        <h5 className="card-title">No movies found</h5>
+                    )}
                 </div>
-            </>
+            </div>
+
         </>
     );
 };
